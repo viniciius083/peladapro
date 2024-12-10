2fd1e517c837199052db412347cee5139f46f1f6
Vinicius <viniciius83@gmail.com>
2024-12-10 12:41:42 -0300

    start project

---
+++ peladapro/src/test/java/com/peladapro/PeladaproApplicationTests.java
@@ -0,0 +1,13 @@
+package com.peladapro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeladaproApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ src/main/java/com/peladapro/PeladaproApplication.java
@@ -0,0 +1,13 @@
+package com.peladapro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeladaproApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeladaproApplication.class, args);
+	}
+
+}
