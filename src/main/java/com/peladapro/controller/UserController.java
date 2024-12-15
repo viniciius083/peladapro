3f4db51b72cc0efc260004aca2187606f56624b0
Vinicius <viniciius83@gmail.com>
2024-12-15 18:21:26 -0300

    addPreAuthorize

--- src/main/java/com/peladapro/controller/UserController.java
+++ src/main/java/com/peladapro/controller/UserController.java
@@ -4,6 +4,7 @@ package com.peladapro.controller;
 import com.peladapro.dto.user.UserDTO;
 import com.peladapro.service.UserService;
 import lombok.AllArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,6 +18,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class UserController {
 
     private final UserService userService;
+
+    @PreAuthorize("hasRole('ADMIN')")
     @PostMapping("/create")
     public String createUser(@ModelAttribute UserDTO userDTO, Model model) {
         userService.createUser(userDTO);
@@ -24,6 +27,7 @@ public class UserController {
         return "redirect:/";
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     @GetMapping("/create")
     public String showCreateUserForm(Model model) {
         model.addAttribute("user", new UserDTO());
