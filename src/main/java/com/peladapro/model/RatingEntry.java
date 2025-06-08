47d35e1720d80de93347faddfbef7e7043c52b3d
Vinicius <viniciius83@gmail.com>
2025-06-08 01:21:31 -0300

    correção de login/usuarios

--- src/main/java/com/peladapro/model/RatingEntry.java
+++ src/main/java/com/peladapro/model/RatingEntry.java
@@ -1,6 +1,9 @@
 package com.peladapro.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -14,6 +17,10 @@ import java.time.LocalDate;
 @Getter
 @Setter
 public class RatingEntry {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
     private String appraiser;
     private Integer rating;
